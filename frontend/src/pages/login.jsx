be96b78254625d7468d5a4b5e395ea211323c45c
Srikanthpolamoni <polamonisrikanth123@gmail.com>
2025-06-16 00:22:03 +0530

    commited

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-     const res = await fetch("https://budgetbloom-app.onrender.com/login", {
+     const res = await fetch("https://budgetbloom-app.onrendercom/login", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
